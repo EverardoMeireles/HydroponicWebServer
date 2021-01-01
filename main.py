eb1478c709a3d8c4455e2553dd47db1d1d34ae2e
Everardo Meireles <everardomeireles@gmail.com>
2021-01-01 23:55:31 +0100

    - added schedule.py and adapted the code for it - replaced instructionList and espIdList by scheduleList - better indentation

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import sqlite3
 from flask import Flask, json, render_template, request
 import datetime;
 import pytz
-
+from schedule import schedule
 
 framesReceive = []
 api = Flask(__name__)
@@ -16,8 +16,7 @@ api = Flask(__name__)
 DATABASE = 'C:\sqlite3\hydroponicDatabase.db'
 
 frameResult = {}
-instructionList = []
-espIdList = []
+scheduleList = []
 
 espHasPendingInstructions = []
 #get all sensor information
@@ -52,26 +51,26 @@ def executeQuery(query):
 
 def prepareToSendInstructions(espid):
     global espHasPendingInstructions
-    global espIdList
-    global instructionList
+    global scheduleList
     #if there are no instructions for this esp "" will be sent
     instructionToSend = ""
-    e = int(espid)
-    b = str(int(espid))
-    c = espHasPendingInstructions
-    if(int(espid) in espHasPendingInstructions):
-        e = int(espid)
-        i = 0
 
-        for id in espIdList:
-            if(id == espid):
-                instructionToSend = instructionList[i]
-                espIdList[i] = None
-                instructionList[i] = None
+    if(int(espid) in espHasPendingInstructions):
+        i = 0
+        for schedule in scheduleList:
+            if(schedule.espid == espid):
+                instructionToSend = schedule.instruction
+                scheduleList[i] = None
                 break
+
             i = i + 1
 
-        if(espIdList.count(espid) == 0):
+        e = 0
+        for schedule in scheduleList:
+            if(schedule != None and schedule.espid == espid):
+                e = e + 1
+
+        if(e == 0):
             espHasPendingInstructions[espHasPendingInstructions.index(espid)] = None
 
     return instructionToSend
@@ -110,12 +109,12 @@ def cutNoneElements(list):
     for element in list:
         if(element != None):
             tempList.append(element)
+
     return tempList
 
 def scheduler():
     cycleCounter = 0
-    global instructionList
-    global espIdList
+    global scheduleList
     while(True):
         ct = datetime.datetime.now(pytz.timezone('Europe/Berlin'))
         #print("current time:-", ct)
@@ -127,36 +126,37 @@ def scheduler():
 
         print(ts)
         if(executeQuery("SELECT MIN(scheduleTimestamp) FROM schedule")[0] == int(ts)):
-            print("espID: " + str(espIdList))
-            print("instructions: " + str(instructionList))
             espIdQuery = "SELECT espid FROM schedule WHERE scheduleTimestamp = " + str(int(ts)) + ";"
             instructionQuery = "SELECT instruction FROM schedule WHERE scheduleTimestamp = " + str(int(ts)) + ";"
+            toDeleteQuery = "SELECT to_delete FROM schedule WHERE scheduleTimestamp = " + str(int(ts)) + ";"
             espIdsForTimestamp = executeQuery(espIdQuery)
             instructionsForTimestamp = executeQuery(instructionQuery);
+            toDeleteForTimestamp = executeQuery(toDeleteQuery)
             if(len(espIdsForTimestamp) > 1):
                 for espId in espIdsForTimestamp:
-                    espIdList.append(espId)
                     if(espId not in espHasPendingInstructions):
                         espHasPendingInstructions.append(espId)
-                for instruction in instructionsForTimestamp:
-                    instructionList.append(instruction)
+
             else:
                 #print("query: " + str(executeQuery("SELECT espid FROM schedule WHERE scheduleTimestamp = " + str(timestamp) + ";")[0]))
                 print(executeQuery(espIdQuery))
-                instructionList.append(executeQuery(instructionQuery)[0])
-                espIdList.append(executeQuery(espIdQuery)[0])
                 #instructionPile[executeQuery("SELECT espid FROM schedule WHERE scheduleTimestamp = " + str(timestamp) + ";")[0]] = executeQuery("SELECT instruction FROM schedule WHERE scheduleTimestamp = " + str(timestamp) + ";")[0]
                 if(executeQuery(espIdQuery)[0] not in espHasPendingInstructions):
                     espHasPendingInstructions.append(executeQuery(espIdQuery)[0])
-            if(executeQuery("SELECT to_delete FROM schedule WHERE scheduleTimestamp = " + str(int(ts)) + ";")[0] == "TRUE"):
+
+            if(executeQuery(toDeleteQuery)[0] == "TRUE"):
                 executeQuery("DELETE FROM schedule WHERE scheduleTimestamp = " + str(int(ts)) + ";")
-            #print(espIdList)
-            #print(instructionList)
+
+            i = 0
+            while (i < len(espIdsForTimestamp)):
+                print(espIdsForTimestamp)
+                scheduleList.append(schedule(espIdsForTimestamp[i], instructionsForTimestamp[i], toDeleteForTimestamp[i]))
+                i = i + 1
 
         if(cycleCounter == 20):
-            instructionList = cutNoneElements(instructionList)
-            espIdList = cutNoneElements(espIdList)
+            scheduleList = cutNoneElements(scheduleList)
             cycleCounter = 0
+
         cycleCounter = cycleCounter + 1
 threadServer = threading.Thread(target=ThreadSocketServer, args=())
 threadServer.start()
---
+++ schedule.py
@@ -0,0 +1,5 @@
+class schedule:
+    def __init__(self, espid, instruction, to_delete):
+        self.espid = espid
+        self.instruction = instruction
+        self.to_delete = to_delete
