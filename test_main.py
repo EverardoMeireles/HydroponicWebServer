f299fda76e79a32d7289ba3e5a5151c1b7c40bdd
everardomeireles@gmail.com <#Dado1241942>
2021-08-26 23:25:11 -0300

    -added a unit test for main.py

---
+++ test_main.py
@@ -0,0 +1,68 @@
+import unittest.mock
+import main
+import utils.database
+import utils.schedule
+import utils.config
+import ujson
+import crawler.pathfinding
+
+
+def update_local_list_of_crawlers():
+    return 0
+
+
+def save_crawlers_file():
+    return 0
+
+
+main.update_local_list_of_crawlers = update_local_list_of_crawlers
+utils.database.save_crawlers_file = save_crawlers_file
+crawler.pathfinding.room_map = \
+    [["Z", "Z", "Z", "Z", "Z", "Z", "Z", "Z", "Z", "Z"],
+     ["Z", "G", "Z", "Z", "G", "Z", "Z", "Z", "G", "Z"],
+     ["Z", "G", "G", "G", "G", "G", "G", "G", "G", "Z"],
+     ["Z", "Z", "G", "G", "G", "Z", "Z", "G", "G", "Z"],
+     ["Z", "Z", "Z", "G", "Z", "Z", "G", "Z", "Z", "Z"],
+     ["Z", "G", "G", "G", "G", "G", "G", "G", "G", "Z"],
+     ["Z", "Z", "Z", "G", "G", "Z", "G", "Z", "G", "Z"],
+     ["Z", "Z", "Z", "G", "Z", "Z", "G", "Z", "Z", "Z"],
+     ["Z", "G", "G", "G", "G", "G", "G", "G", "G", "Z"],
+     ["Z", "Z", "Z", "Z", "Z", "Z", "Z", "Z", "Z", "Z"]]
+
+
+class TestMain(unittest.TestCase):
+    def test_prepare_to_send_instructions(self):
+        # first test
+        main.device_has_pending_instructions = [1]
+        main.schedule_list.append(utils.schedule.Schedule(1,
+                                                          '[{"instruction_type": "move_crawler_to", "destination_y":1, "destination_x":1}]',
+                                                          'FALSE',
+                                                          False,
+                                                          'temperature',
+                                                          1629755709))
+
+        utils.database.list_of_crawlers = ujson.loads(
+            ujson.dumps([{"serial_number": 2, "resting_position_x": 1, "resting_position_y": 8,
+                          "status": "available", "time_started_moving": 16191176745454545458, "coordinates": []}]))
+
+        second_value_compare = '[{"path":["rotate-right","forward","forward","rotate-up","forward","forward","forward",' \
+                               '"forward","forward","rotate-left","forward","rotate-up","forward","rotate-left","forward",' \
+                               '"rotate-up","forward"]}]'
+        self.assertEqual(main.prepare_to_send_instructions(1), second_value_compare)
+
+        # second test
+        utils.database.list_of_crawlers = ujson.loads(
+            ujson.dumps([{"serial_number": 2, "resting_position_x": 1, "resting_position_y": 8,
+                          "status": "available", "time_started_moving": 16191176745454545458, "coordinates": []},
+                         {"serial_number": 2, "resting_position_x": 1, "resting_position_y": 8,
+                          "status": "available", "time_started_moving": 16191176745454545458, "coordinates": []}]))
+
+        main.schedule_list.append(utils.schedule.Schedule(1,
+                                                          '[{"instruction_type": "move_crawler_to", "destination_y":1, "destination_x":1}]',
+                                                          'FALSE',
+                                                          False,
+                                                          'temperature',
+                                                          1629755709))
+        main.device_has_pending_instructions = [1]
+
+        self.assertEqual(main.prepare_to_send_instructions(1), second_value_compare)
