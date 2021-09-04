3c177686bca7e850091c7a3861793200289fe461
everardomeireles@gmail.com <#dado1241942>
2021-09-04 11:16:17 -0300

    - rewrote collision detection in pathfinding.py. - calculate_crawler_path can now be triggered by things other than the scheduler. - the crawler can now request a new path to the main program. - schedules can now reschedule themselves automatically when they are passed, if they are meant to be repeated. - schedules that are only meant to be run one time will auto-delete themselves.

--- utils/instructions.py
+++ utils/instructions.py
@@ -1,4 +1,3 @@
-
 import datetime
 import pytz
 from crawler.pathfinding import PathFinding
@@ -15,7 +14,7 @@ device_has_pending_instructions = []
 # pre-processing of the instruction to be sent according to its type
 def process_instruction_before_sending(instructions):
     instruction_dicts = ast.literal_eval(instructions)
-    processed_instructions = []  #instructions  # is this necessary????
+    processed_instructions = []
     postpone = False
     # if the instruction is about starting the crawler, change the contents of the instruction to the directions
     # it should be moving
@@ -62,10 +61,13 @@ def prepare_to_send_instructions(serial_number):
     return instruction_to_send
 
 
-# randomly selects the crawler to move from available crawlers
-def select_crawler_to_move():
+def select_crawler_to_move(serial_number):
     update_local_list_of_crawlers()
-    list_of_available_crawlers = select_crawler({"status": "available"})
+    # if serial_number is specified, select a specific crawler
+    if serial_number is not None:
+        list_of_available_crawlers = select_crawler({"serial_number": serial_number})
+    else:
+        list_of_available_crawlers = select_crawler({"status": "available"})
     try:
         return list_of_available_crawlers[random.randint(0, len(list_of_available_crawlers) - 1)]
     except ValueError:
@@ -76,11 +78,16 @@ def select_crawler_to_move():
 # get instruction message from the scheduler and calculate the crawler's path
 # used in process_instruction_before_sending(serial_number)
 def calculate_crawler_path(instruction):
-    current_crawler = select_crawler_to_move()
+    instruction_dict = ast.literal_eval(instruction)
+    print("sdqs")
+    crawler_serial_number = None
+    if 'serial_number' in instruction_dict:
+        crawler_serial_number = instruction_dict['serial_number']
+    # make case where the crawler is requesting a new path
+    current_crawler = select_crawler_to_move(crawler_serial_number)
     if current_crawler == "no crawler unavailable":
         return current_crawler
 
-    instruction_dict = ast.literal_eval(instruction)
     starting_position = {"y": current_crawler["resting_position_y"], "x": current_crawler["resting_position_x"]}
     destination = {"y": instruction_dict['destination_y'], "x": instruction_dict['destination_x']}
     path = PathFinding(starting_position, destination)
@@ -88,7 +95,7 @@ def calculate_crawler_path(instruction):
     ct = datetime.datetime.now(pytz.timezone('Europe/Berlin'))
     ts = int(ct.timestamp())
     update_crawler(current_crawler["serial_number"], {"status": "moving",
-                                                      "time_started_moving": ts,
+                                                      "time_started_moving": ts, # plus time offset
                                                       "coordinates": path.direction_coordinates})
     instruction_to_send = ujson.dumps({"path": path.final_directions})
 
@@ -106,9 +113,25 @@ def log_into_database(frame):
 
 # apply processing type based on the type of frame when the frame is supposed to trigger an action
 def process_received_instructions(result):
-    for frame in result:
+    for frame in ujson.loads(ujson.dumps(result)):
         # log into database(sensor data)
         if frame['instruction_type'] in ["temperature", "humidity"]:
             log_into_database(frame)
 
-        # elif
+        # assuming json keys to be: instruction_type, current_position_x, current_position_y, destination_x,
+        # destination_y, serial_number
+        elif frame['instruction_type'] == "request_path":
+            ct = datetime.datetime.now(pytz.timezone('Europe/Berlin'))
+            ts = int(ct.timestamp())
+            starting_position = {"y": frame["current_position_y"], "x": frame["current_position_x"]}
+            destination = {"y": frame['destination_y'], "x": frame['destination_x']}
+            # make this value added to the ts into the config.
+            execute_query("INSERT INTO schedule (schedule_timestamp, instruction, serial_number, to_delete, type, " 
+                          "re_insertion_time_seconds) VALUES (" + str(ts + 5) + ", " + "'" +
+                          "[{" + '"instruction_type"' + ": " + '"move_crawler_to"' + ", " '"destination_y"' + ":"
+                          + str(destination["y"]) + ", " + '"destination_x"' + ": " + str(destination["x"]) + ", "
+                          + '"serial_number"' + ": " + str(frame["serial_number"]) + "}]" + "'" + ", " +
+                          str(frame["serial_number"]) + ", 'TRUE', " + " 'temperature', 0);")
+
+            update_crawler(frame["serial_number"], {"resting_position_x": starting_position["x"],
+                                                    "resting_position_y":starting_position["y"]})
